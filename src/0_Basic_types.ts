c63b44cb099b628bd262d946a29a4fc61ab385bc
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-18 08:37:57 +0530

    basic data types and array

---
+++ src/0_Basic_types.ts
@@ -0,0 +1,13 @@
+//Primitive types:
+
+let userName: string = "Muazim Maqbool"; // string type
+let enroll: number = 6655; // number
+let isAdmin:boolean=true; // for boolean value
+
+//Array
+let numbers:number[]=[1,2,3,4,5]; // array of numbers
+// let numbers:number[]=[1,2,3,4,5,"as"] // this will throw error as string value can't be inside array of number
+
+let names:string[]=["user1","user2","user3"]; // array of strings
+// let names:string[]=["user1","user2","user3",12] // this will throw error as number value can't be inside array of strings
+
