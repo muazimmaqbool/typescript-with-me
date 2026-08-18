848a3cede7a64fe43f5bff74840e52258dc88f7b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-18 09:08:06 +0530

    type inference intro

---
+++ src/1_Type_Inference.ts
@@ -0,0 +1,4 @@
+/*
+Type inference is the TypeScript compiler's ability to automatically determine and assign data types to variables, expressions, and function return values. 
+This happens during compilation when you do not provide explicit type annotations, allowing you to write less code while still maintaining full type safety.
+*/
