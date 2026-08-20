d0dd6598487c62604f7e71dfd225b19f8bf09259
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-20 20:39:44 +0530

    giving types to objects in typescript

---
+++ src/3_Object_Interfaces.ts
@@ -0,0 +1,7 @@
+//Now let's see how do you give types to objects in typescript:
+
+//One method (messy):
+let user:{name:string,enroll:number}={
+    name:"Muazim",
+    enroll:6655, // enroll:"6655" will throw error
+}
