4c9a78c02bc5af8c944c593820c36f6b45956051
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-20 20:43:16 +0530

    interface in ts

--- src/3_Object_Interfaces.ts
+++ src/3_Object_Interfaces.ts
@@ -1,7 +1,20 @@
 //Now let's see how do you give types to objects in typescript:
 
 //One method (messy):
-let user:{name:string,enroll:number}={
-    name:"Muazim",
-    enroll:6655, // enroll:"6655" will throw error
+let student: { name: string; enroll: number } = {
+  name: "Muazim",
+  enroll: 6655, // enroll:"6655" will throw error
+};
+
+//Important better way is to define interface:
+interface User {
+  name: string;
+  enroll: number;
+  isPresent: boolean;
 }
+
+let user: User = {
+  name: "Muazim",
+  enroll: 6655, //enroll:"6655" will throw error
+  isPresent: true,
+};
