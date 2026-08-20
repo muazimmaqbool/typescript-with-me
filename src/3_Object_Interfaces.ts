ca511338384e4a3f19b9038b3c55c9f72431d45f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-20 20:47:30 +0530

    optional in objects

--- src/3_Object_Interfaces.ts
+++ src/3_Object_Interfaces.ts
@@ -11,10 +11,19 @@ interface User {
   name: string;
   enroll: number;
   isPresent: boolean;
+  //extra:
+  email?:string; //now required, it's optional, if email is not optional and then not provided in user object it will throw error in user object, try by removing ? on email
 }
 
 let user: User = {
   name: "Muazim",
   enroll: 6655, //enroll:"6655" will throw error
   isPresent: true,
-};
+//   email:"muazim@gmail.com" // removing this won't throw error
+}
+
+/*
+->An interface in TypeScript is a powerful contract that defines the exact structural "shape" an object must follow. 
+ It enforces compile-time type-checking by establishing a blueprint for properties and methods without providing their actual implementation code
+*/
+//More
