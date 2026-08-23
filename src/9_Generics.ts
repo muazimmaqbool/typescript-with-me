6f78264f4b8b7052495e89a3d7c501bc5b94cdde
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-23 22:53:46 +0530

    generics example one

--- src/9_Generics.ts
+++ src/9_Generics.ts
@@ -7,3 +7,21 @@ Important:
     Instead of freezing a specific type (like string or number) or resorting to type-safety killers like any, 
     generics use a placeholder—conventionally <T>—which gets dynamically defined when the code is executed
 */
+//Example 1:
+function functionOne(arg:any){
+    return arg;
+}
+const output1=functionOne("typescript");
+const output2=functionOne(1234)
+//if you hover over the output1 and output2 you will notice typescript doesn't know what type the function return so outputs will be of any type
+//Now let's use generics
+
+function myFunction<myType>(arg:myType):myType{
+    return arg;
+}
+let one=myFunction("this is string");
+let two=myFunction(3210) // or let two =myFunction<number>(1235)
+//now when you hover on the outputs one and two you will see the proper return type also
+
+//you can also provide explicitly here the type you want in return
+let three=myFunction<boolean>(true)
