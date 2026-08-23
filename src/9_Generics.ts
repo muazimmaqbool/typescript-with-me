4d45dd136373e9495ac2ce1e0908bf957b2be96d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-23 23:02:56 +0530

    generic with arrays

--- src/9_Generics.ts
+++ src/9_Generics.ts
@@ -8,20 +8,31 @@ Important:
     generics use a placeholder—conventionally <T>—which gets dynamically defined when the code is executed
 */
 //Example 1:
-function functionOne(arg:any){
-    return arg;
+function functionOne(arg: any) {
+  return arg;
 }
-const output1=functionOne("typescript");
-const output2=functionOne(1234)
+const output1 = functionOne("typescript");
+const output2 = functionOne(1234);
 //if you hover over the output1 and output2 you will notice typescript doesn't know what type the function return so outputs will be of any type
 //Now let's use generics
 
-function myFunction<myType>(arg:myType):myType{
-    return arg;
+function myFunction<myType>(arg: myType): myType {
+  return arg;
 }
-let one=myFunction("this is string");
-let two=myFunction(3210) // or let two =myFunction<number>(1235)
+let one = myFunction("this is string");
+let two = myFunction(3210); // or let two =myFunction<number>(1235)
 //now when you hover on the outputs one and two you will see the proper return type also
 
 //you can also provide explicitly here the type you want in return
-let three=myFunction<boolean>(true)
+let three = myFunction<boolean>(true);
+
+//Note (important): i have used generic as <myType> but generally <T> is used although it can be any name
+
+//Example 2: generic with arrays
+function getFirst<T>(arr: T[]): T | undefined {
+  // :T | undefined means: it can return either type T or undefined
+  return arr[0];
+}
+let myNum=getFirst([1,2,3])
+let item=getFirst(["abc","def","ghi"]) // defining type is optional: let item=getFirst<string>(["abc","def","ghi"])
+//now when you hover over myNum and item you will see also returned type also with | undefined
