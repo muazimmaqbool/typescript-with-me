d2bed2bbd909650b03bbc30b8045f55e2e70ae43
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-23 11:39:42 +0530

    literal types

--- src/6_Literal_Types.ts
+++ src/6_Literal_Types.ts
@@ -16,3 +16,27 @@ let branch: "CSE" | "Civil" |  "Mechanical" | "Electrical" | "E & C";
 // branch="robotics" //error
 
 //2. Numeric Literal Types:
+type DiceRoll = 1 | 2 | 3 | 4 | 5 | 6;
+
+//function roll will return only values between 1 to 6 as defined in type DiceRoll
+function roll(): DiceRoll {
+    return 4; // Valid 
+    // return 7; // Error: Type '7' is not assignable to type 'DiceRoll' 
+}
+
+//Combining with other types:
+type SuccessResponse={
+    status:"success";
+    data:any;
+}
+type ErrorResponse={
+    status:"error";
+    message:string;
+}
+type APIresponse=SuccessResponse | ErrorResponse;
+let apiResult:APIresponse={
+    status:"success", // can be either success or error,
+    data:"Your api is running",
+    // message:"Api call failed" 
+    //error as status is success the it expects data to be used and if you use message then it will throuw error, as goes if status is error then using data will through error
+}
