64674e2bd25aa65d30fc23add9bf32d80a3096c4
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-23 11:25:19 +0530

    more examples

--- src/5_Union_Intersection.ts
+++ src/5_Union_Intersection.ts
@@ -15,4 +15,39 @@ function getInfo(branch:Branch){ // now this function can take arguments which a
 // getInfo("CSE") //works
 // getInfo("Robotics") //error
 
+//Note type can be like this also:
+type multiple="meString" | true | 6655; // this type is having multiple types
+let test:multiple=true; //works
+// test=false; //error
+
+function printId(id: string | number) {
+  console.log(`Your ID is: ${id}`);
+}
+
+printId(101);     // Valid
+printId("A101");  // Valid
+// printId(true); //error
+
+
 //2-> Intersection Types (AND):
+interface Colorful{
+    color:string;
+}
+interface Circle{
+    radius:Number; // or number
+}
+//now we want both these interfaces to be present in the type
+type ColorfulCircle=Colorful & Circle;
+let myCirlce:ColorfulCircle={
+    color:"Blue",
+    radius:50,
+    //if either color or radius is missing it's gonna through an error
+}
+
+type Person={name:string}
+type UserId={userId:number}
+type Staff=Person & UserId;
+const newStaff: Staff = {
+  name: "Ishfaq",
+  userId: 4412
+};
