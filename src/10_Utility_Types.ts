82f40850a7efa8cb408dfef690fb6e54e0c2ca46
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-24 22:36:30 +0530

    record in ts

--- src/10_Utility_Types.ts
+++ src/10_Utility_Types.ts
@@ -19,6 +19,8 @@ let test: partialTodo = {
 //     title:"Todo one"
 // } //error
 
+// ---------------------------------------------------------------------------------------
+
 // 2: Required - makes all the properties as required
 type RequiredTodo = Required<Todo>;
 //now if you hover on it you will see all fields are now required
@@ -37,6 +39,8 @@ let test3: RequiredTodo = {
 //     assignedTo:"user1"
 // } // works fine
 
+// ---------------------------------------------------------------------------------------
+
 // 3: Readonly - makes all properties readonly
 type ReadonlyTodo = Readonly<Todo>;
 let myTodo: ReadonlyTodo = {
@@ -50,23 +54,62 @@ let myTodo: ReadonlyTodo = {
 
 // myTodo.completed=true; //error when used like this: let myTodo: ReadonlyTodo = {...}
 
+// ---------------------------------------------------------------------------------------
+
 // 4: Pick : pick specific properties
 //suppose you want to make another todo with only with title and completed:
 //one way:
-    // type todoNew={
-    //   title:string,
-    //   completed:boolean
-    // }
+// type todoNew={
+//   title:string,
+//   completed:boolean
+// }
 
 //another way we can pick just two prot=perties from Todo interface:
-type NewTodo=Pick<Todo,"title"|"completed">
+type NewTodo = Pick<Todo, "title" | "completed">;
 //now when you hover over NewTodo you will see it has two properties only title and completed
-let todo2:NewTodo={
-    title:"tasknew",
-    completed:true
-}
+let todo2: NewTodo = {
+  title: "tasknew",
+  completed: true,
+};
+
+// ---------------------------------------------------------------------------------------
 
 // 5: Omit : omit specific properties
 //it's opposite of pick, here you want to create another todo but same Properties as Todo interface but without createdAt property
-type TodoUpdated=Omit<Todo,"createdAt"> // or type TodoUpdated=Omit<Todo,"createdAt" | "title">
+type TodoUpdated = Omit<Todo, "createdAt">; // or type TodoUpdated=Omit<Todo,"createdAt" | "title">
 //now hover over TodoUpdated
+
+// ---------------------------------------------------------------------------------------
+
+//this is one of the coolest feature of typescript
+// 6: Record : Record<Keys, Type> is a built-in utility type used to map the properties of an object to a specific data type.
+//    It allows you to quickly create an object type with predefined key types and value types
+
+type PageInfo = {
+  title: string;
+  url: string;
+};
+type Pages = "home" | "about" | "contact";
+
+//now i want to merge these two i.e PageInfo and Pages into one single type so that Pages becomes keys and PageInfo as the value of each key
+type Merged = Record<Pages, PageInfo>;
+//Now when you hover over Merged you will see like this:
+                                                        // type merged = {
+                                                        //     home: PageInfo;
+                                                        //     about: PageInfo;
+                                                        //     contact: PageInfo;
+                                                        // }
+let pages: Merged = {
+  home: {
+    title: "Home",
+    url: "/",
+  },
+  about: {
+    title: "About",
+    url: "/about",
+  },
+  contact: {
+    title: "Contact",
+    url: "/contact",
+  },
+};
