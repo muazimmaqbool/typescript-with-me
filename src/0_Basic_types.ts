9df69e565ec43d270711205807df4605f48bbaf5
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-08-27 17:25:31 +0530

    basic types in ts

--- src/0_Basic_types.ts
+++ src/0_Basic_types.ts
@@ -2,10 +2,11 @@
 let userName: string = "Muazim Maqbool"; // string type
 let enroll: number = 6655; // number
 let isAdmin: boolean = true; // for boolean value
+// isAdmin="true" //error
 
 //Array:
 let numbers: number[] = [1, 2, 3, 4, 5]; // array of numbers
-// let numbers:number[]=[1,2,3,4,5,"as"] // this will throw error as string value can't be inside array of number
+// let numbers:number[]=[1,2,3,4,5,"as"] // this will throw error as string value can't be inside array of numbers
 
 let names: string[] = ["user1", "user2", "user3"]; // array of strings
 // let names:string[]=["user1","user2","user3",12] // this will throw error as number value can't be inside array of strings
@@ -97,6 +98,6 @@ function height(num:number):number{
 
 //Null and undefined in ts:
 let nullValue:null=null;
-// let undefinedValue:undefined=undefined;
+let undefinedValue:undefined=undefined;
 // let undefinedValue:undefined=null; //will throw error
 // let undefinedValue:undefined="value"; //will throw error
